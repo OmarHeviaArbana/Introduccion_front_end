055827bf5cd5f56a639c2be76b1c199c11a64873
OmarHeviaArbana <ohevia@uoc.edu>
2024-03-16 17:23:20 +0100

    Introduce the following improvements in the script of exercise PEC1_Ej2_2.:
    -When querying the API, indicate waiting state with a message
    
    -Do not allow negative numbers
    
    -If an error occurs when querying the API, display a message with the error.

--- PEC1_Ej2_2/script.js
+++ PEC1_Ej2_2/script.js
@@ -2,28 +2,38 @@ const currencyEl_one = document.getElementById('currency-one');
 const amountEl_one = document.getElementById('amount-one');
 const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
-
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-// Fetch exchange rates and update the DOM
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
+  const amount_one = parseFloat(amountEl_one.value);
+  
+  rateEl.innerText = 'Calculating exchange rates...';
+
+  if (amount_one < 0 || isNaN(amount_one)) {
+    alert("Please enter a positive amount.");
+    return;
+  }
 
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then(res => res.json())
+  .then(res => {
+    if (res.status != 200) {
+      throw new Error('Some error occurred while exchange rates was calculating.Please, try again.');
+    }
+    return res.json();
+    })
     .then(data => {
-      // console.log(data);
       const rate = data.rates[currency_two];
-
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    })
+    .catch(error => {
+      rateEl.innerText = `Error: ${error.message}`;
     });
 }
 
-// Event listeners
 currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
